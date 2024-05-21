07102574b89439a8995d0a1230aad586d57d6cba
alijitx <a.li@jitx.com>
2024-05-21 02:58:38 -0700

    Add depth option for libgit2_clone

--- src/libgit2.c
+++ src/libgit2.c
@@ -2,18 +2,18 @@
 #include <git2.h>
 
 // Wrapper function to handle the options structures outside of stanza
+// Creates a clone of the repo at 'url' in 'local_path' with the given depth
+// Set depth = 0 for full (normal) clone
 // Assumes library has already been initialized with git_libgit2_init()
 // Out param git_repository must be freed with git_repository_free()
 // Returns the same return code as git_clone()
-int stz_libgit2_clone(git_repository **out, const char *url, const char *local_path)
+int stz_libgit2_clone(git_repository **out, const char *url, const char *local_path, int depth)
 {
-  int return_code = 1;
   git_clone_options clone_opts = GIT_CLONE_OPTIONS_INIT;
-  git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
 
   /* Set up options */
-  checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
-  clone_opts.checkout_opts = checkout_opts;
+  clone_opts.checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
+  clone_opts.fetch_opts.depth = depth;
 
   /* Do the clone */
   return git_clone(out, url, local_path, &clone_opts);
